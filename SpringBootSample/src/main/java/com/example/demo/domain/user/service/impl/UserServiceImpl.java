ec013bc46fe19e80c9e2ba01e67e9e6321a98936
Satoshi T <sulp91_fy@yahoo.co.jp>
2022-02-13 11:43:04 +0900

    認証機能追加

--- SpringBootSample/src/main/java/com/example/demo/domain/user/service/impl/UserServiceImpl.java
+++ SpringBootSample/src/main/java/com/example/demo/domain/user/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.domain.user.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.user.model.MUser;
@@ -15,12 +16,19 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserMapper mapper;
 	
+	@Autowired
+	private PasswordEncoder encoder;
+	
 	/** ユーザー登録 */
 	@Override
 	public void signup(MUser user) {
 		// SignupFormが持っていないプロパティをセットする。
 		user.setDepartmentId(1);
 		user.setRole("ROLE_GENERAL");
+		
+		// パスワード暗号化
+		String rawPasswd = user.getPassword();
+		user.setPassword(encoder.encode(rawPasswd));
 		
 		//TODO トランザクション制御は？
 		mapper.insertOne(user);
@@ -40,7 +48,7 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public void updateUserOne(String userId, String password, String userName) {
-		mapper.updateOne(userId, password, userName);
+		mapper.updateOne(userId, encoder.encode(password), userName);
 	}
 
 	@Override
@@ -48,4 +56,10 @@ public class UserServiceImpl implements UserService {
 		var count = mapper.deleteOne(userId);
 	}
 
+	@Override
+	public MUser getLoginUser(String userId) {
+		return mapper.findLoginUser(userId);
+	}
+
+	
 }
